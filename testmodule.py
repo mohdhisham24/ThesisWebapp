f52a9314282f62680054de2b4370fc5329f880be
Hisham <mohammedhisham897@gmail.com>
2024-07-19 00:38:23 +0100

    steering wheel rotatory module added-v6 - added module tester script

--- testmodule.py
+++ testmodule.py
@@ -33,41 +33,10 @@ try:
         
         clkLastState = clkState
         time.sleep(0.01)
-finally:
-    GPIO.cleanup()import RPi.GPIO as GPIO
-import time
 
-# Set up GPIO using BCM numbering
-GPIO.setmode(GPIO.BCM)
+except KeyboardInterrupt:
+    print("\nExiting...")
 
-# Define pin numbers
-CLK = 2
-DT = 3
-SW = 4
-
-# Set up pins as inputs
-GPIO.setup(CLK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(DT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(SW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-clkLastState = GPIO.input(CLK)
-
-try:
-    while True:
-        clkState = GPIO.input(CLK)
-        dtState = GPIO.input(DT)
-        swState = GPIO.input(SW)
-        
-        if clkState != clkLastState:
-            if dtState != clkState:
-                print("Clockwise")
-            else:
-                print("Counterclockwise")
-        
-        if swState == GPIO.LOW:
-            print("Button pressed")
-        
-        clkLastState = clkState
-        time.sleep(0.01)
 finally:
     GPIO.cleanup()
+    print("GPIO cleaned up")
