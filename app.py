843544aba12c0aa20e4868c626939ca7bb25253d
Hisham <mohammedhisham897@gmail.com>
2024-08-12 18:30:57 +0100

    cube v6

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ if __name__ == '__main__':
         print("Starting Flask application")
         socketio.run(app, host='0.0.0.0', port=5000, debug=False)
     except Exception as e:
-        print(f"Error running
+        print(f"Error running")
